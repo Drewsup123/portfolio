f815ea614d51c534eea16d7d21d958de5bdb5d9e
Drew <drewsup123c@gmail.com>
2019-02-14 15:23:00 -0700

    added css reset onto project, started work on landing page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,24 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(){
     super();
-    this.state = []
+    this.state = {
+      words : ['Web Developer', 'Software Engineer', 'Programmer', 'Designer'],
+      selected : 0,
+    }
   }
+
+  componentDidMount(){
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Hello My Name is Drew <br /> 
+            I am a 
+            <div className="change-text">{this.state.words[this.state.selected]}<span className="blinker"></span></div>
+          </h1>
         </header>
       </div>
     );
