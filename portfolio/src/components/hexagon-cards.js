c2034b89dd9be2d272131d21ce8deb381dfa9278
Drew <drewsup123c@gmail.com>
2019-02-23 13:42:35 -0700

    added another card

--- portfolio/src/components/hexagon-cards.js
+++ portfolio/src/components/hexagon-cards.js
@@ -23,7 +23,29 @@ class Hexagon extends React.Component{
                         <div className = {this.state.hoverClass}>
                             <p>React, Node.JS, Express.JS</p>
                             <a 
-                                href="http://www.github.com/Drewsup123" 
+                                href="https://github.com/Drewsup123/front-end-project-week" 
+                                target="_blank">
+                                <i className='fab fa-github project-icon'></i>
+                                <i className="fas fa-globe project-icon"></i>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div 
+                    className="box2 projects2" 
+                    onMouseEnter={()=>this.setState({hoverClass : ''})}
+                    onMouseLeave={()=>this.setState({hoverClass : 'hidden'})}
+                >
+                    <div className="box-content">
+                        <p>S&J Architects</p>
+                        <hr />
+
+                        <div className = {this.state.hoverClass}>
+                            <p>HTML, CSS/LESS, JavaScript</p>
+                            <a 
+                                href="https://github.com/Drewsup123/front-end-project-week" 
                                 target="_blank">
                                 <i className='fab fa-github project-icon'></i>
                                 <i className="fas fa-globe project-icon"></i>
@@ -33,6 +55,7 @@ class Hexagon extends React.Component{
                     </div>
                 </div>
             </section>
+            
         );
     }
 }
