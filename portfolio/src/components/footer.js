107538a398a522a18153caeaa9cf7477af887b25
Drew <drewsup123c@gmail.com>
2019-03-01 23:15:00 -0700

    added viewport meta tag, and basic styling/boilerplate for hd, tablet, and phone views

--- portfolio/src/components/footer.js
+++ portfolio/src/components/footer.js
@@ -6,10 +6,10 @@ class Footer extends React.Component{
         return(
             <div className="footer">
                 <div className="footer-content">
-                    <h3><strong>Drew Johnson</strong></h3>
                     <h3>Email : drewsup123c@gmail.com</h3>
                     <h3>Github: http://github.com/Drewsup123</h3>
                     <h3>LinkedIn: https://www.linkedin.com/in/drew-johnson-99ab34171/</h3>
+                    <a href="https://resume.creddle.io/resume/hjhco45g57w" target="_blank" rel="noopener noreferrer">Resume</a>
                     <Link to="header" smooth={true}>
                         <i className="fa fa-chevron-up"></i>
                     </Link>
