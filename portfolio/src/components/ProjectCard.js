3c5c3de8b5011c9418c350fb72951ece432464e8
Drew <drewsup123c@gmail.com>
2023-08-17 17:32:20 -0600

    Updated project cards to be a reusable component

---
+++ portfolio/src/components/ProjectCard.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+const ProjectCard = (props) => {
+    const {
+        href,
+        title,
+        description,
+        stack,
+        githubLink,
+        websiteLink,
+        backgroundUrl,
+    } = props;
+    const [hovering, setHovering] = useState(false);
+
+    return (
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="box2 projectsH"
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+            style={{
+                background: `url("${backgroundUrl}")`,
+            }}
+        >
+            <div className="box-content">
+                <p className="card-title">{title}</p>
+                <hr />
+                <p className="card-desc">{description}</p>
+                <div className={hovering ? "" : "hidden"}>
+                    <p className="project-stack">{stack}</p>
+                    <a
+                        href={githubLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fab fa-github project-icon" />
+                    </a>
+                    <a
+                        rel="noopener noreferrer"
+                        href={websiteLink}
+                        target="_blank"
+                    >
+                        <i className="fas fa-globe project-icon" />
+                    </a>
+                </div>
+            </div>
+        </a>
+    );
+};
+
+export default ProjectCard;
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,112 +1,57 @@
-import React from 'react';
+import React from "react";
 import "./hexagon.css";
+import ProjectCard from "./ProjectCard";
 
-class Projects extends React.Component{
-    constructor(){
+class Projects extends React.Component {
+    constructor() {
         super();
         this.state = {
-            hoverClass : 'hidden',
-            hoverClass2 : 'hidden',
-            hoverClass3 : 'hidden',
-            hoverClass4: 'hidden',
-            hoverClass5 : 'hidden'
-        }
+            hoverClass: "hidden",
+            hoverClass2: "hidden",
+            hoverClass3: "hidden",
+            hoverClass4: "hidden",
+            hoverClass5: "hidden",
+        };
     }
-    render(){
-        return(
-            <div className="project-section" >
-                <a
+    render() {
+        return (
+            <div className="project-section">
+                <ProjectCard
                     href="https://www.decentralizeddrive.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="box2 projects5 projectsH" 
-                    onMouseEnter={()=>this.setState({hoverClass5 : ''})}
-                    onMouseLeave={()=>this.setState({hoverClass5 : 'hidden'})}
-                >
-                    <div className="box-content">
-                        <p className="card-title">Decentralized Drive</p>
-                        <hr />
-                        <p className="card-desc">A decentralized Google Drive like application that allows users to create and import documents, images, pdfs, and JavaScript files</p>
-                        <div className = {this.state.hoverClass5}>
-                            <p className="project-stack">React, Redux, Material-UI, Blockstack</p>
-                            <a 
-                                href="https://github.com/Drewsup123/BlockStack-App" 
-                                target="_blank" rel="noopener noreferrer">
-                                <i className='fab fa-github project-icon'></i>
-                            </a>
-                            <a 
-                            rel="noopener noreferrer"
-                            href="https://www.decentralizeddrive.com/"
-                            target="_blank">
-                                <i className="fas fa-globe project-icon"></i>
-                            </a>
-                            
-                        </div>
-
-                    </div>
-                </a>
-                <a
+                    title="Decentralized Drive"
+                    description="A decentralized Google Drive like application that allows users to create and import documents, images, pdfs, and JavaScript files"
+                    stack="React, Redux, Material-UI, Blockstack"
+                    githubLink="https://github.com/Drewsup123/BlockStack-App"
+                    websiteLink="https://www.decentralizeddrive.com/"
+                    backgroundUrl="https://i.imgur.com/S7OmElZ.png"
+                />
+                <ProjectCard
                     href="https://swooblz.netlify.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="box2 projects4 projectsH" 
-                    onMouseEnter={()=>this.setState({hoverClass4 : ''})}
-                    onMouseLeave={()=>this.setState({hoverClass4 : 'hidden'})}
-                >
-                    <div className="box-content">
-                        <p className="card-title">Swooblz</p>
-                        <hr />
-                        <p className="card-desc">A Slack clone with chat groups and private messaging functionality</p>
-                        <div className = {this.state.hoverClass4}>
-                            <p className="project-stack">React, Redux, Firebase Realtime Database, SemanticUI, CSS</p>
-                            <a 
-                                href="https://github.com/Drewsup123/Swooblz-official" 
-                                target="_blank" rel="noopener noreferrer">
-                                <i className='fab fa-github project-icon'></i>
-                            </a>
-                            <a 
-                            rel="noopener noreferrer"
-                            href="https://swooblz.netlify.com/"
-                            target="_blank">
-                                <i className="fas fa-globe project-icon"></i>
-                            </a>
-                            
-                        </div>
-
-                    </div>
-                </a>
-
-                <a 
+                    title="Swooblz"
+                    description="A Slack clone with chat groups and private messaging functionality"
+                    stack="React, Redux, Firebase Realtime Database, SemanticUI, CSS"
+                    githubLink="https://github.com/Drewsup123/Swooblz-official"
+                    websiteLink="https://swooblz.netlify.com/"
+                    backgroundUrl="https://i.ibb.co/QQHk5q5/2019-05-07.png"
+                />
+                <ProjectCard
                     href="https://learned-app.now.sh/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="box2 projects3 projectsH" 
-                    onMouseEnter={()=>this.setState({hoverClass3 : ''})}
-                    onMouseLeave={()=>this.setState({hoverClass3 : 'hidden'})}
-                >
-                    <div className="box-content">
-                        <p className="card-title">Erudition</p>
-                        <hr />
-                        <p className="card-desc">Erudition is a social learning network that allows users to save and share articles
-                        and courses that have read or taken</p>
-                        <div className = {this.state.hoverClass3}>
-                            <p className="project-stack">React, React Context/Hooks, NextJS, Firebase, NodeJs, Express</p>
-                            <a 
-                                href="https://github.com/labs11-learned-1/labs11-learned1-FE" 
-                                target="_blank" rel="noopener noreferrer">
-                                <i className='fab fa-github project-icon'></i>
-                            </a>
-
-                            <a 
-                                href="https://learned-app.now.sh/" 
-                                target="_blank" rel="noopener noreferrer">
-                                <i className="fas fa-globe project-icon"></i>
-                            </a>
-                        </div>
-
-                    </div>
-                </a>
-
+                    title="Erudition"
+                    description="Erudition is a social learning network that allows users to save and share articles and courses that have read or taken"
+                    stack="React, React Context/Hooks, NextJS, Firebase, NodeJs, Express"
+                    githubLink="https://github.com/labs11-learned-1/labs11-learned1-FE"
+                    websiteLink="https://learned-app.now.sh/"
+                    backgroundUrl="https://i.ibb.co/wphPM0K/Erudition-Site2.jpg"
+                />
+                <ProjectCard
+                    href="https://s-and-j-architects.netlify.com/index.html"
+                    title="S&J Architects"
+                    description="A simple webpage with full responsiveness and pure JavaScript functionality"
+                    stack="HTML, CSS/LESS, JavaScript"
+                    githubLink="https://github.com/Drewsup123/front-end-project-week"
+                    websiteLink="https://s-and-j-architects.netlify.com/index.html"
+                    backgroundUrl="https://i.ibb.co/x2hG9Z5/sandj.png"
+                />
                 {/* <a
                     href="https://5c847bf7921b68cdd3d10dc2--drews-note-taking-react-app.netlify.com/"
                     target="_blank"
@@ -138,39 +83,7 @@ class Projects extends React.Component{
 
                     </div>
                 </a> */}
-
-                <a
-                    href="https://s-and-j-architects.netlify.com/index.html"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="box2 projects2 projectsH" 
-                    onMouseEnter={()=>this.setState({hoverClass2 : ''})}
-                    onMouseLeave={()=>this.setState({hoverClass2 : 'hidden'})}
-                >
-                    <div className="box-content">
-                        <p className="card-title">S&J Architects</p>
-                        <hr />
-                        <p className="card-desc">A simple webpage with full responsiveness and pure JavaScript functionality</p>
-                        <div className = {this.state.hoverClass2}>
-                            <p className="project-stack">HTML, CSS/LESS, JavaScript</p>
-                            <a 
-                                href="https://github.com/Drewsup123/front-end-project-week" 
-                                target="_blank" rel="noopener noreferrer">
-                                <i className='fab fa-github project-icon'></i>
-                            </a>
-                            <a 
-                            rel="noopener noreferrer"
-                            href="https://s-and-j-architects.netlify.com/index.html"
-                            target="_blank">
-                                <i className="fas fa-globe project-icon"></i>
-                            </a>
-                            
-                        </div>
-
-                    </div>
-                </a>
             </div>
-            
         );
     }
 }
